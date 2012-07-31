23a023dc60daae64c455390e6c21b21db1a06337
Rogerio Prado de Jesus <c066770@mail.caixa>
2012-07-31 14:45:42 -0300

    Include routes 'books' and 'book/:id'
    Signed-off-by: Rogerio Prado de Jesus <c066770@mail.caixa>

--- index.php
+++ index.php
@@ -1,10 +1,57 @@
 <?php
 require 'vendor/Slim/Slim.php';
+require 'vendor/NotORM/NotORM.php';
 
-$app = new Slim();
+$dsn = 'sqlite:' . realpath('./data/db.sqlite');
+
+$pdo = new PDO($dsn);
+$db = new NotORM($pdo);
+
+$app = new Slim(
+    array(
+        'MODE' => 'development',
+        'TEMPLATES.PATH' => './templates'
+    )
+);
 
 $app->get('/', function() {
     echo '<h1>Hello Slim World</h1>';
+});
+
+$app->get('/books', function() use ($app, $db) {
+    $books = array();
+    foreach ($db->books() as $book) {
+        $books[] = array(
+            'id' => $book['id'],
+            'title' => $book['title'],
+            'author' => $book['author'],
+            'summary' => $book['summary']
+        );
+    }
+    $app->response()->header('Content-Type', 'application/json');
+    echo json_encode($books);
+});
+
+$app->get('/book/:id', function() use ($app, $db) {
+    $app->response()->header('Content-Type', 'application/json');
+    $book = $db->books()->where('id', $id);
+    if ($data = $book->fetch()) {
+        echo json_encode(
+            array(
+                'id' => $data['id'],
+                'title' => $data['title'],
+                'author' => $data['author'],
+                'summary' => $data['summary']
+            )
+        );
+    } else {
+        echo json_encode(
+            array(
+                'status' => false,
+                'message' => 'Book ID ' . $id . ' does not exist'
+            )
+        );
+    }
 });
 
 $app->run();
